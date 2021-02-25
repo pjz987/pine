4ae29ef2cecd899ef13dc566ae67528e4f39e364
Pete Jones <pwj2012@gmail.com>
2021-02-24 18:42:57 -0600

    save state

---
+++ Pine/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionExit(Collision other) {
+        audioSource.Play()
+    }
+    void Update()
+    {
+        
+    }
+}
