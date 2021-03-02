2b031d83dfebf5bfefa592d60c4176f4b0387d53
socknot <38506114+seancunnington@users.noreply.github.com>
2021-03-02 15:41:16 -0800

    CharacterParticleEffects: only creates particles on sapling growth.

---
+++ Pine/Assets/Scripts/CharacterParticleEffects.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class CharacterParticleEffects : MonoBehaviour
+{
+     public GameObject vfxPineconeSaplingGrowth;
+     public GameObject vfxTreeFlick;
+     public GameObject vfxPineconeGroundImpact;
+
+     public List<GameObject> currentSystems = new List<GameObject>();
+
+
+     private void Update()
+     {
+          // If the list of particle systems is 0, don't do anything.
+          if (currentSystems.Count == 0)
+               return;
+
+          // For every particle system, check if any particles exist for that system. If not, destroy that object.
+          for (int i = 0; i < currentSystems.Count; i++)
+          {
+               if (currentSystems[i].GetComponent<ParticleSystem>().IsAlive() == false)
+               {
+                    GameObject systemToDestroy = currentSystems[i];
+                    currentSystems.Remove(currentSystems[i]);         // Remove from list
+                    Destroy(systemToDestroy);                         // Destroy object
+               }
+          }
+     }
+
+
+     bool CheckVFX(GameObject vfx)
+     {
+          // Check if a vfx object is attached.
+          if (vfx == null)
+          {
+               Debug.Log("Failed to play vfx: No vfx object attached.");
+               return false;
+          }
+
+          // Check if a particle system is attached to the object.
+          ParticleSystem vfxCurrent = vfx.GetComponent<ParticleSystem>();
+          if (vfxCurrent == null)
+          {
+               Debug.Log("Failed to play vfx: No particle system attached to vfx object.");
+               return false;
+          }
+
+          // If both checks were passed, then able to play vfx.
+          return true;
+     }
+
+
+
+     public void PlayVFX(GameObject vfx, float strength = 0f)
+     {
+          // Check if any vfx is attached.
+          if (CheckVFX(vfx) == false)
+               return;
+
+          // Create the new particle system and add it to the list of systems.
+          GameObject newSystem = Instantiate(vfx, transform.position, Quaternion.identity);
+          currentSystems.Add(newSystem);
+
+          ParticleSystem vfxCurrent = newSystem.GetComponent<ParticleSystem>();
+          int emitAmount = vfxCurrent.emission.burstCount;
+          vfxCurrent.Emit(emitAmount);
+     }
+}
